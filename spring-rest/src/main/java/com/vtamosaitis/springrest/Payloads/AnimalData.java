6338b13a5614d8f9ccbe3bd49b6202dccd0cb763
Vince Tamosaitis <vince@pop-os.localdomain>
2022-04-02 14:52:09 -0500

    reformat

--- spring-rest/src/main/java/com/vtamosaitis/springrest/Payloads/AnimalData.java
+++ spring-rest/src/main/java/com/vtamosaitis/springrest/Payloads/AnimalData.java
@@ -6,5 +6,14 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 public class AnimalData {
 	public String name;
 	public Long specie_id;
-	public Long enc_id;	
+	public Long enc_id;
+
+	@Override
+	public String toString() {
+		return "AnimalData{" +
+				"name='" + name + '\'' +
+				", specie_id=" + specie_id +
+				", enc_id=" + enc_id +
+				'}';
+	}
 }
--- spring-rest/src/main/java/com/vtamosaitis/springrest/SpringRestApplication.java
+++ spring-rest/src/main/java/com/vtamosaitis/springrest/SpringRestApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringRestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringRestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestApplication.class, args);
+    }
 
 }
